077db381b125c42cf6eb8c55a91f56b2de3329d3
Anastasiia Matveeva <eglekato@gmail.com>
2021-10-29 23:16:16 +0300

    modified build.gradle, WebTest

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,28 +14,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChromeTest {
 
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setupTest() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    public static class WebTest {
+public class WebTest {
         private WebDriver driver;
 
         @BeforeAll
@@ -47,7 +26,12 @@ class ChromeTest {
 
         @BeforeEach
         void setUp() {
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
             driver.get("http://localhost:7777");
         }
 
@@ -58,38 +42,32 @@ class ChromeTest {
         }
 
         @Test
-        void Test1() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--headless");
-            driver = new ChromeDriver(options);
+        void shouldOrderCardWithoutCssSelectors() {
 
             List<WebElement> elements = driver.findElements(By.className("input__control"));
             elements.get(0).sendKeys("Василий");
             elements.get(1).sendKeys("+79870000000");
             driver.findElement(By.className("checkbox__box")).click();
             driver.findElement(By.className("button")).click();
-            String text = driver.findElement(By.className("order-success")).getText();
-            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время", text.strip());
+            String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.strip());
+
+
         }
 
         @Test
-        void Test2() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--headless");
-            driver = new ChromeDriver(options);
+        void shouldOrderCardWithCssSelectors() {
 
             driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий");
             driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79290000000");
-            driver.findElement(By.cssSelector("checkbox__box")).click();
+            driver.findElement(By.cssSelector(".checkbox__box")).click();
             driver.findElement(By.cssSelector("button")).click();
-            String text = driver.findElement(By.className("order-success")).getText();
-            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время", text.trim());
+            String text = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText();
+            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+
         }
     }
-}
+
 
 
